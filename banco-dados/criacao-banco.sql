9ff99a7ef084cc63be4f115a73a1e661a2a3c25d
FelipeSteffens <felipesteffens2019@outlook.com>
2024-12-04 14:06:20 -0300

    .

--- banco-dados/criacao-banco.sql
+++ banco-dados/criacao-banco.sql
@@ -1,20 +1,20 @@
 
---  CREATE TABLE usuarios(
--- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- nome VARCHAR(245) NOT NULL,
--- email VARCHAR(245) NOT NULL,
--- senha VARCHAR(12) NOT NULL,
--- celular VARCHAR(14) NOT NULL,
--- data_nascimento VARCHAR(45) NOT NULL,
--- cep VARCHAR(9) NOT NULL
--- );
+ CREATE TABLE usuarios(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(245) NOT NULL,
+email VARCHAR(245) NOT NULL,
+senha VARCHAR(12) NOT NULL,
+celular VARCHAR(14) NOT NULL,
+data_nascimento VARCHAR(45) NOT NULL,
+cep VARCHAR(9) NOT NULL
+);
 
 CREATE TABLE produtos(
 id_produtos INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nome_produto VARCHAR(245) NOT NULL,
 preco_produto VARCHAR(300) NOT NULL,
 descricao_produto VARCHAR(500) NOT NULL,
-foto_produto VARCHAR(500) NOT NULL,
+foto_produto LONGTEXT NOT NULL,
 usuarios_id INT NOT NULL,
 FOREIGN KEY (usuarios_id) REFERENCES usuarios(id)
 );
--- database/ConexaoMySql.js
+++ database/ConexaoMySql.js
@@ -1,20 +1,20 @@
 import mysql from "mysql2/promise";
 
-// const dbConfig = {
-//   host: process.env.MYSQL_HOST || "localhost",
-//   port: process.env.MYSQL_PORT || "3306",
-//   user: process.env.MYSQL_USER || "root",
-//   password: process.env.MYSQL_PWD || "senai",
-//   database: process.env.MYSQL_DB || "banco_dados_sa",
-// };
-
 const dbConfig = {
-  host: process.env.MYSQL_HOST || "autorack.proxy.rlwy.net",
-  port: process.env.MYSQL_PORT || "16631",
+  host: process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_PORT || "3306",
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PWD || "oxZYSEnfkPSjyhbNtrEhqWslcpZicyGY",
-  database: process.env.MYSQL_DB || "railway",
+  password: process.env.MYSQL_PWD || "senai",
+  database: process.env.MYSQL_DB || "banco_dados_sa",
 };
+
+// const dbConfig = {
+//   host: process.env.MYSQL_HOST || "autorack.proxy.rlwy.net",
+//   port: process.env.MYSQL_PORT || "16631",
+//   user: process.env.MYSQL_USER || "root",
+//   password: process.env.MYSQL_PWD || "oxZYSEnfkPSjyhbNtrEhqWslcpZicyGY",
+//   database: process.env.MYSQL_DB || "railway",
+// };
 
 
 class ConexaoMySql {
