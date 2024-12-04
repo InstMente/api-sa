9fbb6911dc72ba33eb8a0c91eff9ab6bf889bae5
Gatti Programador Junior <167263639+EduardGatti@users.noreply.github.com>
2024-12-04 17:45:53 -0300

    Delete src/comum directory

--- src/comum/servicos/ServicoProduto.js
+++
@@ -1,9 +0,0 @@
-import instanciaApi from "./Api"
-
-class ServicoProduto {
-    cadastrarProduto(produto) {
-        return instanciaApi.post('/cadastrar-produto', produto)
-    }
-}
-
-export default ServicoProduto;
