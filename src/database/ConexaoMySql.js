7b157c4bb0342a2bf82f084684047cc70c6b25c4
Gatti Programador Junior <167263639+EduardGatti@users.noreply.github.com>
2024-12-04 17:50:00 -0300

    Add files via upload

---
+++ src/database/ConexaoMySql.js
@@ -0,0 +1,28 @@
+import mysql from "mysql2/promise";
+
+const dbConfig = {
+  host: process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_PORT || "3306",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PWD || "senai",
+  database: process.env.MYSQL_DB || "banco_dados_sa",
+};
+
+// const dbConfig = {
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.MYSQL_USER, 
+//   password: process.env.MYSQL_PWD, 
+//   database: process.env.MYSQL_DB
+// };
+
+
+class ConexaoMySql {
+  async getConexao() {
+    if (!ConexaoMySql.conexao) {
+      ConexaoMySql.conexao = await mysql.createConnection(dbConfig);
+      return ConexaoMySql.conexao;
+    }
+  }
+}
+export default ConexaoMySql;
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import cors from "cors";
+import express from "express";
+import UsuariosController from "./controllers/UsuariosController.js";
+import AutenticacaoController from "./controllers/AutenticacaoController.js";
+import ProdutoController from "./controllers/ProdutoController.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+//Rota publicas
+const _usuariosController = new UsuariosController();
+const _autenticacaoController = new AutenticacaoController();
+const _produtoController = new ProdutoController();
+
+
+app.post("/login", _autenticacaoController.login);
+app.post("/usuarios", _usuariosController.adicionar);
+app.post("/cadastrar-produto", _produtoController.adicionarProduto);
+app.get("/produto", _produtoController.listar);
+
+
+// app.use((req, resp, next) => {
+//   const usuarioLogado = req.headers["x-usuario"]
+//   if(!usuarioLogado){
+//     resp.status(401).send();
+//     return;
+//   }
+//   next();
+// })
+  
+
+
+// Rotas privadas
+app.get("/usuarios", _usuariosController.listar);
+app.put("/usuarios", _usuariosController.atualizar);
+app.delete("/usuarios/:id", _usuariosController.excluir);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`API está rodando na porta ${port}`);
+});
