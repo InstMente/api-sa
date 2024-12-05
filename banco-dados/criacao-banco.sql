629949d14830b35c79c9638f4480aae75cbd6c6b
FelipeSteffens <felipesteffens2019@outlook.com>
2024-12-05 16:33:57 -0300

    comando banco de dados reformulado

--- banco-dados/criacao-banco.sql
+++ banco-dados/criacao-banco.sql
@@ -3,8 +3,8 @@
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(245) NOT NULL,
 email VARCHAR(245) NOT NULL,
-senha VARCHAR(12) NOT NULL,
-celular VARCHAR(14) NOT NULL,
+senha VARCHAR(50) NOT NULL,
+celular VARCHAR(50) NOT NULL,
 data_nascimento VARCHAR(45) NOT NULL,
 cep VARCHAR(9) NOT NULL
 );
@@ -15,6 +15,6 @@ nome_produto VARCHAR(245) NOT NULL,
 preco_produto VARCHAR(300) NOT NULL,
 descricao_produto VARCHAR(500) NOT NULL,
 foto_produto LONGTEXT NOT NULL,
-usuarios_id INT NOT NULL,
-FOREIGN KEY (usuarios_id) REFERENCES usuarios(id)
+usuario_id INT NOT NULL,
+FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
