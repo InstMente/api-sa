ec2b269c5ea6bb276a2e53b53c313fcbd1e3360f
Gatti Programador Junior <eduardogatti600@gmail.com>
2024-12-05 21:55:43 -0300

    att produto controller

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -1,77 +1,88 @@
 import ConexaoMySql from "../database/ConexaoMySql.js";
 
 class ProdutoController {
-    async adicionarProduto(req, resp) {
-        const usuarioLogado = req.headers["x-usuario"]
-        try {
-            const novoProduto = req.body;
-            if (!novoProduto.nomeProduto || !novoProduto.descricaoProduto || !novoProduto.valorProduto || !novoProduto.fotoProduto) {
-                resp.status(400).send("Preencha todos os campos.");
-                return;
-            }
-            const conexao = await new ConexaoMySql().getConexao();
-            if (!conexao) {
-                console.error("Erro na conexão com o banco de dados.");
-                resp.status(500).send("Erro na conexão com o banco de dados.");
-                return;
-            }
-            const comandoSql = `
+  async adicionarProduto(req, resp) {
+    const usuarioLogado = req.headers["x-usuario"]
+    try {
+      const novoProduto = req.body;
+      if (!novoProduto.nomeProduto || !novoProduto.descricaoProduto || !novoProduto.valorProduto || !novoProduto.fotoProduto) {
+        resp.status(400).send("Preencha todos os campos.");
+        return;
+      }
+      const conexao = await new ConexaoMySql().getConexao();
+      if (!conexao) {
+        console.error("Erro na conexão com o banco de dados.");
+        resp.status(500).send("Erro na conexão com o banco de dados.");
+        return;
+      }
+      const comandoSql = `
                 INSERT INTO produtos (nome_produto, preco_produto, descricao_produto, foto_produto, usuario_id)
                 VALUES (?, ?, ?, ?, ?)
             `;
-            const [resultado] = await conexao.execute(comandoSql, [
-                novoProduto.nomeProduto,
-                novoProduto.valorProduto,
-                novoProduto.descricaoProduto,
-                novoProduto.fotoProduto,
-                usuarioLogado
-            ]);
-            resp.send({ message: "Produto adicionado com sucesso!", resultado });
-        } catch (error) {
-            console.error("Erro ao adicionar produto:", error);
-            if (error.code === 'ER_DUP_ENTRY') {
-                resp.status(400).send("Produto já cadastrado.");
-                return;
-            }
-            resp.status(500).send({
-                message: "Erro no servidor. Tente novamente mais tarde.",
-                erro: error.message
-            });
-        }
-    }
-    async list(req, resp) {
-        try {
-          const usuarioLogado = req.headers["x-usuario"]
-          console.log(usuarioLogado);
-    
-          const conexao = await new ConexaoMySql().getConexao();
-          const comandoSql = `SELECT * FROM produtos p WHERE p.usuario_id = ${usuarioLogado}`;
-    
-          const filtro = req.query.filtro || "";
-          const [resultado] = await conexao.execute(comandoSql, [`%${filtro}%`]);
-          resp.send(
-            resultado
-          );
-        } catch (error) {
-          resp.status(500).send(error);
-        }
-      } 
-
-    async listar(req, resp) {
-        try {
-          
-          const conexao = await new ConexaoMySql().getConexao();
-          const comandoSql = `SELECT * FROM produtos `;
-    
-          const filtro = req.query.filtro || "";
-          const [resultado] = await conexao.execute(comandoSql, [`%${filtro}%`]);
-          resp.send(
-            resultado
-          );
-        } catch (error) {
-          resp.status(500).send(error);
-        }
+      const [resultado] = await conexao.execute(comandoSql, [
+        novoProduto.nomeProduto,
+        novoProduto.valorProduto,
+        novoProduto.descricaoProduto,
+        novoProduto.fotoProduto,
+        usuarioLogado
+      ]);
+      resp.send({ message: "Produto adicionado com sucesso!", resultado });
+    } catch (error) {
+      console.error("Erro ao adicionar produto:", error);
+      if (error.code === 'ER_DUP_ENTRY') {
+        resp.status(400).send("Produto já cadastrado.");
+        return;
       }
+      resp.status(500).send({
+        message: "Erro no servidor. Tente novamente mais tarde.",
+        erro: error.message
+      });
+    }
+  }
+  async list(req, resp) {
+    try {
+      const usuarioLogado = req.headers["x-usuario"]
+      console.log(usuarioLogado);
+
+      const conexao = await new ConexaoMySql().getConexao();
+      const comandoSql = `SELECT * FROM produtos p WHERE p.usuario_id = ${usuarioLogado}`;
+
+      const filtro = req.query.filtro || "";
+      const [resultado] = await conexao.execute(comandoSql, [`%${filtro}%`]);
+      resp.send(
+        resultado
+      );
+    } catch (error) {
+      resp.status(500).send(error);
+    }
+  }
+
+  async listar(req, resp) {
+    try {
+
+      const conexao = await new ConexaoMySql().getConexao();
+      const comandoSql = `SELECT * FROM produtos `;
+
+      const filtro = req.query.filtro || "";
+      const [resultado] = await conexao.execute(comandoSql, [`%${filtro}%`]);
+      resp.send(
+        resultado
+      );
+    } catch (error) {
+      resp.status(500).send(error);
+    }
+  }
+  async excluirProduto(req, resp) {
+    try {
+      const conexao = await new ConexaoMySql().getConexao();
+      const comandoSql = "DELETE FROM produtos WHERE id = ?";
+      const [resultado] = await conexao.execute(comandoSql, [+req.params.id]);
+
+      resp.send(resultado);
+    } catch (error) {
+      resp.status(500).send(error);
+    }
+  }
 }
 
 export default ProdutoController;
